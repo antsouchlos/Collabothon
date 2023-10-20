0ea9ae1c0e15e4d7b5c2cc931c0b9b5229dbec88
Toastbytes <johntsouchlos@gmail.com>
2023-10-20 21:01:55 +0200

    First UI

--- carbongecko/lib/main.dart
+++ carbongecko/lib/main.dart
@@ -1,125 +1,239 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Define the gecko green color
+    const Color geckoGreen = Color(0xFF6CBB3C);
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Carbon Gecko',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: geckoGreen,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              // Using Expanded to let this part take the top 2/3rds of the screen.
+              Expanded(
+                flex: 2, // occupies 2/3rds of the space because total (2 + 1) = 3 and 2/3 are two parts
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 150,
+                      child: Center(
+                        child: Image.asset(
+                          'assets/geckoLogo.png',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Carbon Gecko',
+                      style: GoogleFonts.acme(
+                        fontSize: 30,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              // Spacer will occupy the remaining space, pushing the fields and button to the end of the column.
+              Spacer(),
+              // Email input field.
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email Address',
+                    hintText: 'Enter your email',
+                  ),
+                ),
+              ),
+              // Password input field.
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                child: TextField(
+                  obscureText: true,  // This will obscure text input for passwords.
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                  ),
+                ),
+              ),
+              // Button at the bottom
+              Builder(
+                builder: (BuildContext context) { // This context is aware of the Navigator
+                  return Container(
+                    padding: const EdgeInsets.symmetric(vertical: 20.0),
+                    width: double.infinity, // makes the container full width
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const SecondScreen()),
+                        );
+                      },
+                      child: const Text(
+                        'Start',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        onPrimary: geckoGreen,
+                        padding: const EdgeInsets.all(20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0), // making corners completely square
+                        ),
+                        minimumSize: Size(double.infinity, 50),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          // This Flexible widget contains your title and pie chart, and takes up 2/3 of the screen.
+          Flexible(
+            flex: 2, // 2/3 of the space
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Today',
+                    style: TextStyle(
+                      // Make sure you're using the correct font name here
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24), // For spacing
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white, // Background color of the container
+                    borderRadius: BorderRadius.circular(20), // Adjust the value as required for the rounded corners.
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                  height: 220,
+                  width: 220,
+                  child: PieChart(
+                    PieChartData(
+                      // Your PieChartData settings here.
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 40,
+                      startDegreeOffset: -90,
+                      sections: _getSections(), // The actual sections data
+                      // Define behavior and style of the chart here
+                      // Other PieChartData properties...
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // This takes up 1/3 of the screen space, and it's empty for now. You can later add other widgets in this part.
+          Flexible(
+            flex: 1, // 1/3 of the space
+            child: Container(), // Add other widgets here based on your design
+          ),
+        ],
+      ),
+    );
+  }
+
+  // This function will return the 'sections' of the pie chart
+  List<PieChartSectionData> _getSections() {
+    // For demonstration, I'm using arbitrary values for each section
+    // You should update these with your actual data
+
+    return List.generate(4, (i) { // Assuming you have 4 sections
+      final isTouched = false;
+      final double fontSize = isTouched ? 25 : 16;
+      final double radius = isTouched ? 60 : 50;
+
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: Colors.red, // Define the appropriate color for this section
+            value: 25, // Replace with actual data value
+            title: '25%', // Replace with actual data label
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: Colors.blue, // Define the appropriate color for this section
+            value: 25, // Replace with actual data value
+            title: '25%', // Replace with actual data label
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 2:
+          return PieChartSectionData(
+            color: Colors.blue, // Define the appropriate color for this section
+            value: 25, // Replace with actual data value
+            title: '25%', // Replace with actual data label
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        default:
+          return PieChartSectionData();
+      }
+    });
   }
 }
